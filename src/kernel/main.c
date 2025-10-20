65510c6c43dc64d48b463cec4e599b1754b1e204
Drawer Xu <xez976446306@163.com>
2025-10-20 14:26:43 +0800

    更新原始结构

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -2,41 +2,36 @@
 #include "lib/mod.h"
 #include "mem/mod.h"
 #include "trap/mod.h"
+#include "proc/mod.h"
 
-volatile int cpu0_ready = 0;
-volatile int cpu0_first_dida = 0;     
+volatile static int started = 0;
 
-int main() {
+int main()
+{
     int cpuid = r_tp();
 
     if (cpuid == 0) {
+
+        print_init();
         printf("cpu %d is booting!\n", cpuid);
-        trap_kernel_init(); 
-        timer_update(); 
-        cpu0_first_dida = 1; 
+
+        pmem_init();
+        kvm_init();
+        kvm_inithart();
+        trap_kernel_init();
+        trap_kernel_inithart();
+        proc_make_first();
         __sync_synchronize();
-        cpu0_ready = 1;
+        started = 1;
     } else {
-        while (cpu0_ready == 0) {
-            asm volatile("nop");
-        }
-        while (cpu0_first_dida == 0) {
-            asm volatile("nop");
-        }
-    }
 
-    trap_kernel_inithart();
-    
-    if (cpuid == 1) {
+        while (started == 0)
+            ;
+        __sync_synchronize();
         printf("cpu %d is booting!\n", cpuid);
-        timer_update();  // 输出CPU1的di da
+        kvm_inithart();
+        trap_kernel_inithart();
     }
-    // uint64 sstatus = r_sstatus();
-    // sstatus |= SSTATUS_SIE;  // 最后开启中断总开关
-    // w_sstatus(sstatus);
-
-    while (1) {
-        asm volatile("wfi");
-    }
-    return 0;
+    while (1)
+        ;
 }
